cd2e834dd0f4e2d95c4db736cb996d6a33aba814
DeAnna Nichols <nicholsdeanna12@gmail.com>
2025-10-23 17:51:00 -0500

    Create xula driver file including space for Tester's function

---
+++ xula_driver.py
@@ -0,0 +1,11 @@
+
+
+
+
+def main():
+    pass
+
+#TODO: The Tester must create and call a function that scrapes the Centennial 
+# Campaign Impact from XULA's website.
+if __name__ == "__main__":
+    main()
