ea78f439047dd31e38feefc280004bde81d75af9
DeAnna Nichols <nicholsdeanna12@gmail.com>
2025-10-28 13:42:23 -0500

    Add files from imdb python data scraping repo

--- xula_driver.py
+++ xula_driver.py
@@ -3,9 +3,8 @@
 
 
 def main():
-    pass
 
-#TODO: The Tester must create and call a function that scrapes the Centennial 
-# Campaign Impact from XULA's website.
+    #TODO: The Tester must create and call a function that scrapes the Centennial 
+    # Campaign Impact from XULA's website.
 if __name__ == "__main__":
     main()
