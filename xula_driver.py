3bee55e9719e67b1668adec25ce71771996efc99
DeAnna Nichols <nicholsdeanna12@gmail.com>
2025-11-05 16:13:52 -0600

    Implementing Movie class in the driver

--- xula_driver.py
+++ xula_driver.py
@@ -5,6 +5,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from requests import get
 import lxml
+from Movie import Movie
 
 def main():
 
@@ -26,10 +27,21 @@ def main():
         imdb_page = IMDB(html_content)
         movie_df = imdb_page.movieData()
         all_movies.append(movie_df)
+    
 
     full_df = pd.concat(all_movies, ignore_index=True)
     print("Here is our movie data for ", user_input + ":")
     print(full_df[user_input].to_string(index=False))
+
+    specific_input = input('Would you like to see a specific movie's data? (yes/no): ').strip().lower()
+    if specific_input == 'yes':
+        movie_title = input('Enter the movie title: ').strip()
+        specific_movie = full_df[full_df['Title'].str.lower() == movie_title.lower()]
+        if not specific_movie.empty:
+            print(specific_movie.to_string(index=False))
+        else:
+            print("Movie not found.")
+
     
 if __name__ == "__main__":
     main()
