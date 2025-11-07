adea405382570fcc9ff4be719c653b0276d26c4f
Janya ward <janyaward33@gmail.com>
2025-11-07 08:26:01 -0600

    Test that movies returned are by specified director

---
+++ test_filter_director.py
@@ -0,0 +1,29 @@
+import unittest
+import pandas as pd
+from director_file import DirectorFilter
+
+class TestDirectorFilter(unittest.TestCase):
+
+    def setUp(self):
+        self.data = pd.DataFrame({
+        "Title": ["Inception", "Interstellar", "Tenet", "Dunkirk", "Pulp Fiction", "Jackie Brown"],
+        "Director": ["Christopher Nolan", "Christopher Nolan","Christopher Nolan","Christopher Nolan","Quentin Tarantino","Quentin Tarantino"],
+        "Genre": ["Action", "Sci-Fi", "Thriller", "War", "Crime", "Drama"],
+        "Rating": [8.8, 8.6, 7.4, 7.9, 8.9, 7.5]
+    })
+        self.filter = DirectorFilter(self.data)
+
+
+    def test_filter_valid_director(self):
+        data = {
+            "Title": ["Inception", "Interstellar", "Dunkirk"],
+            "Director": ["Christopher Nolan", "Christopher Nolan", "Someone Else"]
+        }
+    
+    df = pd.DataFrame(data)
+    filter_obj = DirectorFilter(df)
+
+    result = filter_obj.filter_by_director("Christopher Nolan")
+
+    self.assertEqual(len(result), 2)
+    self.assertTrue(all(result["Director"] == "Christopher Nolan"))
