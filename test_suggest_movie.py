4f4c07a14bd1c0bafbf2800e3af6eb265341e516
Janya ward <janyaward33@gmail.com>
2025-11-07 07:55:41 -0600

    Add test for adding new movie

--- test_suggest_movie.py
+++ test_suggest_movie.py
@@ -10,3 +10,9 @@ class TestSuggestMovie(unittest.TestCase):
     def test_suggest_random_movie(self):
         result = self.suggester.suggest_random()
         self.assertIn(result, self.movie_list, "Suggested movie should be from the list")
+
+    def test_add_movie(self):
+        self.suggester.add_movie("Interstellar")
+        self.assertIn("Interstellar", self.suggester.get_all_movies(), "New movie should be added")
+
+    
